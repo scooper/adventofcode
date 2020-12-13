93ccb2d9263e5ac9586710faf877055d36918f55
Sam <samuel.j.cooper@outlook.com>
2020-12-13 22:57:42 +0000

    Day 10 Part 2 but it's wrong, so not complete

--- 2020/Day10/src/main.cpp
+++ 2020/Day10/src/main.cpp
@@ -7,8 +7,8 @@
 int main()
 {
 
-    AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day10/input.txt");
-    //AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day10/inputTest.txt");
+    //AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day10/input.txt");
+    AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day10/inputTest.txt");
     //AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day10/inputTest2.txt");
 
     std::vector<int> adapters;
@@ -48,12 +48,47 @@ int main()
 
     }
 
+    // output
     std::cout << "1 Jolt Differences: " << diff1 << std::endl;
     std::cout << "2 Jolt Differences: " << diff2 << std::endl;
     std::cout << "3 Jolt Differences: " << diff3 << std::endl;
 
     std::cout << "1JD x 3JD = " << diff1 * diff3 << std::endl;
 
+    // sort top to bottom
+    std::sort(adapters.begin(), adapters.end(), [](const int& a, const int& b) { return a > b; });
+
+    int validTotal = 1;
+
+    for (std::vector<int>::const_iterator it = adapters.begin(); it != adapters.end()-1; it++)
+    {
+        int validOptions = 0;
+        for (std::vector<int>::const_iterator it2 = it + 1; it2 != adapters.end(); it2++)
+        {
+
+            int targetJoltage = *it;
+            int testJoltage = *it2;
+
+            // get joltage difference
+            if ((targetJoltage - testJoltage) > 3)
+            {
+                break;
+            }
+            else
+            {
+                validOptions++;
+            }
+        }
+
+        validTotal *= validOptions;
+    }
+
+
+    std::cout << "Total Valid Adapter Configurations: " << validTotal << std::endl;
+
 
     return 0;
 }
+
+// TODO: write recursive function that traverses tree of possible options in a depth-first-search manner
+//       look at notes taken to refresh memory of how to approach the problem
