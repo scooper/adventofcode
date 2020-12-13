7014ceb61291dc45ad66e43a282852f80c0790d2
Sam <samuel.j.cooper@outlook.com>
2020-12-13 20:37:07 +0000

    Day 11 Part 2 complete

--- 2020/Day11/src/main.cpp
+++ 2020/Day11/src/main.cpp
@@ -8,15 +8,20 @@
 
 int main()
 {
-    //AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day11/input.txt");
-    AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day11/inputTest.txt");
+    AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day11/input.txt");
+    //AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day11/inputTest.txt");
 
     std::vector<std::string> input = AdventCommon::String::Split(file.AsStr(), "\n");
 
+    // part 1
     WaitingArea wa(input);
 
     std::cout << "Num occupied when pattern doesn't change (old rules): " << wa.ApplyOldRules() << std::endl;
-    std::cout << "Num occupied when pattern doesn't change (new rules): " << wa.ApplyNewRules() << std::endl;
+
+    // part 2
+    WaitingArea wa2(input);
+
+    std::cout << "Num occupied when pattern doesn't change (new rules): " << wa2.ApplyNewRules() << std::endl;
 
     return 0;
 }
