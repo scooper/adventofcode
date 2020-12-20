9527fff5e6339bcd8d1c46a064b447a71143bace
Sam <samuel.j.cooper@outlook.com>
2020-12-20 23:42:41 +0000

    Day 13 Part 2 - not verified so may need work

--- 2020/Day13/src/main.cpp
+++ 2020/Day13/src/main.cpp
@@ -11,30 +11,14 @@ struct BusTime
     int waitTime;
 };
 
-int main()
+struct BusListing
 {
-    AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day13/input.txt");
-    //AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day13/inputTest.txt");
+    int id;
+    bool unrestricted;
+};
 
-    std::string line;
-    file.NextLine(line);
-
-    int earliestDepartTime = atoi(line.c_str());
-    
-    file.NextLine(line);
-
-    std::vector<std::string> busIDStrings = AdventCommon::String::Split(line, ",");
-    std::vector<int> busIDs;
-
-
-    for (std::string id : busIDStrings)
-    {
-        if (AdventCommon::String::IsNumber(id))
-        {
-            busIDs.push_back(atoi(id.c_str()));
-        }
-    }
-
+void Part1(std::vector<int>& busIDs, int& earliestDepartTime)
+{
     BusTime earlistBusTime = { 0,0,0 };
 
     for (int i = earliestDepartTime; i > 0; i++)
@@ -53,7 +37,75 @@ int main()
     }
 
     std::cout << "Bus ID (" << earlistBusTime.busId << ") at time " << earlistBusTime.time << " with wait time "
-              << earlistBusTime.waitTime << " gives " << earlistBusTime.waitTime * earlistBusTime.busId << std::endl;
+        << earlistBusTime.waitTime << " gives " << earlistBusTime.waitTime * earlistBusTime.busId << std::endl;
+    
+    return;
+}
+
+// WARNING: SLOW
+void Part2(std::vector<BusListing> listings)
+{
+    for (long long i = 1; i > 0; i++)
+    {
+        // TODO: rethink loop end condition as we want to check if we get to the end of
+        //       the bus listings list - not leave the loop (maybe use iterators)
+        for (long long j = 0; j < listings.size(); j++)
+        {
+            long long minute = j + i;
+            if (listings[j].unrestricted)
+                continue;
+            if (minute % listings[j].id != 0)
+            {
+                break;
+            }
+
+            if (j == listings.size() - 1)
+            {
+                std::cout << "Earliest minute fulfilling conditions is " << i << std::endl;
+                i = -1;
+                break;
+            }
+
+            // if complete, outer loop -1 and break while adding the earliest time stamp info (i)
+        }
+    }
+
+    return;
+}
+
+int main()
+{
+    AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day13/input.txt");
+    //AdventCommon::File file("D:/Projects/adventofcode/2020/inputs/Day13/inputTest.txt");
+
+    std::string line;
+    file.NextLine(line);
+
+    int earliestDepartTime = atoi(line.c_str());
+    
+    file.NextLine(line);
+
+    std::vector<std::string> busIDStrings = AdventCommon::String::Split(line, ",");
+
+    std::vector<int> busIDs;
+    std::vector<BusListing> busListings;
+
+    for (std::string id : busIDStrings)
+    {
+        if (AdventCommon::String::IsNumber(id))
+        {
+            int idNum = atoi(id.c_str());
+            busIDs.push_back(idNum);
+            busListings.push_back({ idNum, false });
+        }
+        else
+        {
+            busListings.push_back({ -1,true });
+        }
+    }
+
+    Part1(busIDs, earliestDepartTime);
+    Part2(busListings);
 
     return 0;
 }
