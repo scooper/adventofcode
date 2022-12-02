16afc2138e38a5918f8b696ebe733d6f906bfa91
Sam Cooper <samuel.j.cooper@outlook.com>
2022-12-02 12:12:50 +0900

    completed day 1 part 1

--- 2022/src/day1/mod.rs
+++ 2022/src/day1/mod.rs
@@ -3,11 +3,40 @@ use std::path::PathBuf;
 
 pub struct Day1 ();
 
+struct Elf {
+    num: i32,
+    calories: i32,
+}
+
 impl Challenge for Day1 {
     fn run(&self, filepath: PathBuf) {
-        println!("Day 1 Challenge \n Part 1:");
+        println!("-------- Day 1 Challenge ---------\nPart 1:");
         let input = file_to_string(filepath);
 
-        println!("{}", input);
+        let input_split = input.split("\r\n");
+
+        let mut elf_num: i32 = 1;
+        let mut elves: Vec<Elf> = vec![Elf { num: elf_num, calories: 0 }];
+
+        for line in input_split {
+            if line.trim().is_empty() {
+                elf_num +=1;
+                elves.push(Elf { num: elf_num, calories: 0 });
+                continue;
+            }
+
+            let len = elves.len();
+            elves[len-1].calories += line.parse::<i32>().unwrap();
+        }
+
+        let mut most_caloried_elf: &Elf = elves.first().unwrap();
+        for (_, elf) in elves.iter().enumerate() {
+            println!("Elf {} calories: {}", elf.num, elf.calories);
+            if elf.calories > most_caloried_elf.calories {
+                most_caloried_elf = elf;
+            }
+        }
+
+        println!("Elf {} has most calories @ {}", most_caloried_elf.num, most_caloried_elf.calories);
     }
 }
